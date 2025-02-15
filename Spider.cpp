ca78a167c034d5e38e0f60c2a418df6dec16334d
Ricky Rosales <rickyiros123@gmail.com>
2025-02-14 22:33:07 -0600

    Initial commit

---
+++ Spider.cpp
@@ -0,0 +1,8 @@
+// Spider.cpp
+#include "Spider.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <fstream>
+#include <iomanip>
+// #include "Spider.h"
+
+using namespace std;
+
+int main(){
+    // vector<Spider*> spiderCollection;
+}
